f6496c1b50cc45b5ef00e3299f243bf4d0ca918d
vvzvlad <vvzvlad@gmail.com>
2017-01-06 05:53:37 +0300

    cosmetic

--- examples/unwired_smarthome/project-conf.h
+++ examples/unwired_smarthome/project-conf.h
@@ -41,17 +41,17 @@
 #define PROJECT_CONF_H_
 /*---------------------------------------------------------------------------*/
 /* Disable button shutdown functionality */
-#define BUTTON_SENSOR_CONF_ENABLE_SHUTDOWN    0 //??????
+#define BUTTON_SENSOR_CONF_ENABLE_SHUTDOWN       0 //??????
 /*---------------------------------------------------------------------------*/
 #undef IEEE802154_CONF_PANID
 #define IEEE802154_CONF_PANID					0xABCD
 #undef RF_CORE_CONF_CHANNEL
-#define RF_CORE_CONF_CHANNEL					26
+#define RF_CORE_CONF_CHANNEL					    26
 
 #undef RF_BLE_CONF_ENABLED
 #define RF_BLE_CONF_ENABLED						0
 #undef STARTUP_CONF_VERBOSE
-#define STARTUP_CONF_VERBOSE					1
+#define STARTUP_CONF_VERBOSE					    1
 
 /* MAC tune option */
 #undef NETSTACK_CONF_MAC
@@ -59,9 +59,9 @@
 #undef NETSTACK_CONF_RDC
 #define NETSTACK_CONF_RDC						contikimac_driver //nullrc_driver
 #undef NETSTACK_CONF_FRAMER
-#define NETSTACK_CONF_FRAMER					framer_802154 //framer_nullmac
+#define NETSTACK_CONF_FRAMER					    framer_802154 //framer_nullmac
 #undef NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE
-#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE	8 //in Hz, 2, 4, 8, 16, 32...
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE	    8 //in Hz, 2, 4, 8, 16, 32...
 
 /* Encryption */
 #undef LLSEC802154_CONF_ENABLED
@@ -73,15 +73,15 @@
 #undef NONCORESEC_CONF_SEC_LVL
 #define NONCORESEC_CONF_SEC_LVL           		0x05
 #undef LLSEC802154_CONF_SECURITY_LEVEL
-#define LLSEC802154_CONF_SECURITY_LEVEL 		0x05
+#define LLSEC802154_CONF_SECURITY_LEVEL 		    0x05
 #undef NONCORESEC_CONF_KEY
 #define NONCORESEC_CONF_KEY						{0xF3,0x01,0x02,0x03,0x04,0x05,0x07,0x07,0x06,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F}
 
 /* Bootloader */
-#define SET_CCFG_BL_CONFIG_BOOTLOADER_ENABLE        0xC5 // 0xC5 - Enable ROM boot loader, 0x00 disable
-#define SET_CCFG_BL_CONFIG_BL_LEVEL                 0x0  // Active low level to open boot loader backdoor
-#define SET_CCFG_BL_CONFIG_BL_PIN_NUMBER            0x01 // DIO number 1 for boot loader backdoor
-#define SET_CCFG_BL_CONFIG_BL_ENABLE                0xC5 // 0xC5 - Enabled boot loader backdoor, 0xFF disable
+#define SET_CCFG_BL_CONFIG_BOOTLOADER_ENABLE     0xC5 // 0xC5 - Enable ROM boot loader, 0x00 disable
+#define SET_CCFG_BL_CONFIG_BL_LEVEL              0x0  // Active low level to open boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_PIN_NUMBER         0x01 // DIO number 1 for boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_ENABLE             0xC5 // 0xC5 - Enabled boot loader backdoor, 0xFF disable
 
 /*---------------------------------------------------------------------------*/
 
