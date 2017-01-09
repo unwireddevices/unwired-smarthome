591b3a4ca0bc04009e4e1f6b07711199b0c4dec0
vvzvlad <vvzvlad@gmail.com>
2017-01-09 08:29:42 +0300

    add black ud-relay project

---
+++ examples/unwired_smarthome/ud-relay.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2016, Unwired Devices LLC. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+ /*---------------------------------------------------------------------------*/
+ /*
+ * \file
+ *         UDP reporting button service for Unwired Devices mesh smart house system(UDMSHS %) <- this is smile
+ * \author
+ *         Vladislav Zaytsev vvzvlad@gmail.com vz@unwds.com
+ */
+ /*---------------------------------------------------------------------------*/
+
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+#include "net/ip/uip.h"
+#include "net/rpl/rpl.h"
+
+#include "net/netstack.h"
+#include "uip-ds6-route.h"
+#include "net/ip/uip-debug.h"
+#include "dev/leds.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "button-sensor.h"
+#include "cc26xx/board.h"
+#include "board-peripherals.h"
+#include "dev/watchdog.h"
+#include "simple-udp.h"
+
+#include "ud-relay.h"
+#include "ud-dag_node.h"
+
+#include "ti-lib.h"
+#include "ud_binary_protocol.h"
+
+/*---------------------------------------------------------------------------*/
+#define DEBUG 1
+#include "net/ip/uip-debug_UD.h"
+/*---------------------------------------------------------------------------*/
+SENSORS(&button_e_sensor);
+PROCESS(ud_relay_process, "Relay control process"); //register main button process
+AUTOSTART_PROCESSES(&ud_relay_process, &dag_node_process); //set autostart processes
+/*---------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*/
+
+PROCESS_THREAD(ud_relay_process, ev, data)
+{
+  PROCESS_BEGIN();
+  PRINTF("Unwired relay device. HELL-IN-CODE free. I hope.\n");
+
+  PROCESS_PAUSE();
+  
+  while(1) {
+    PROCESS_YIELD();
+
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
