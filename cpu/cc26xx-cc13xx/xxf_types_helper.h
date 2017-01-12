f7a2e8cf84ad308d8455cc08658202f6e2a49b77
vvzvlad <vvzvlad@gmail.com>
2017-01-12 10:26:21 +0300

    add types helper define

---
+++ cpu/cc26xx-cc13xx/xxf_types_helper.h
@@ -0,0 +1,329 @@
+#define PRId16   "d"
+//decimal printf format for int16_t
+
+#define PRId32   "ld"
+//decimal printf format for int32_t
+
+#define PRId8   "d"
+//decimal printf format for int8_t
+
+#define PRIdFAST16   "d"
+//decimal printf format for int_fast16_t
+
+#define PRIdFAST32   "ld"
+//decimal printf format for int_fast32_t
+
+#define PRIdFAST8   "d"
+//decimal printf format for int_fast8_t
+
+#define PRIdLEAST16   "d"
+//decimal printf format for int_least16_t
+
+#define PRIdLEAST32   "ld"
+//decimal printf format for int_least32_t
+
+#define PRIdLEAST8   "d"
+//decimal printf format for int_least8_t
+
+#define PRIdPTR   PRId16
+//decimal printf format for intptr_t
+
+#define PRIi16   "i"
+//integer printf format for int16_t
+
+#define PRIi32   "li"
+//integer printf format for int32_t
+
+#define PRIi8   "i"
+//integer printf format for int8_t
+
+#define PRIiFAST16   "i"
+//integer printf format for int_fast16_t
+
+#define PRIiFAST32   "li"
+//integer printf format for int_fast32_t
+
+#define PRIiFAST8   "i"
+//integer printf format for int_fast8_t
+
+#define PRIiLEAST16   "i"
+//integer printf format for int_least16_t
+
+#define PRIiLEAST32   "li"
+//integer printf format for int_least32_t
+
+#define PRIiLEAST8   "i"
+//integer printf format for int_least8_t
+
+#define PRIiPTR   PRIi16
+//integer printf format for intptr_t
+
+#define PRIo16   "o"
+//octal printf format for uint16_t
+
+#define PRIo32   "lo"
+//octal printf format for uint32_t
+
+#define PRIo8   "o"
+//octal printf format for uint8_t
+
+#define PRIoFAST16   "o"
+//octal printf format for uint_fast16_t
+
+#define PRIoFAST32   "lo"
+//octal printf format for uint_fast32_t
+
+#define PRIoFAST8   "o"
+//octal printf format for uint_fast8_t
+
+#define PRIoLEAST16   "o"
+//octal printf format for uint_least16_t
+
+#define PRIoLEAST32   "lo"
+//octal printf format for uint_least32_t
+
+#define PRIoLEAST8   "o"
+//octal printf format for uint_least8_t
+
+#define PRIoPTR   PRIo16
+//octal printf format for uintptr_t
+
+#define PRIu16   "u"
+//decimal printf format for uint16_t
+
+#define PRIu32   "lu"
+//decimal printf format for uint32_t
+
+#define PRIu8   "u"
+//decimal printf format for uint8_t
+
+#define PRIuFAST16   "u"
+//decimal printf format for uint_fast16_t
+
+#define PRIuFAST32   "lu"
+//decimal printf format for uint_fast32_t
+
+#define PRIuFAST8   "u"
+//decimal printf format for uint_fast8_t
+
+#define PRIuLEAST16   "u"
+//decimal printf format for uint_least16_t
+
+#define PRIuLEAST32   "lu"
+//decimal printf format for uint_least32_t
+
+#define PRIuLEAST8   "u"
+//decimal printf format for uint_least8_t
+
+#define PRIuPTR   PRIu16
+//decimal printf format for uintptr_t
+
+#define PRIx16   "x"
+//hexadecimal printf format for uint16_t
+
+#define PRIX16   "X"
+//uppercase hexadecimal printf format for uint16_t
+
+#define PRIx32   "lx"
+//hexadecimal printf format for uint32_t
+
+#define PRIX32   "lX"
+//uppercase hexadecimal printf format for uint32_t
+
+#define PRIx8   "x"
+//hexadecimal printf format for uint8_t
+
+#define PRIX8   "X"
+//uppercase hexadecimal printf format for uint8_t
+
+#define PRIxFAST16   "x"
+//hexadecimal printf format for uint_fast16_t
+
+#define PRIXFAST16   "X"
+//uppercase hexadecimal printf format for uint_fast16_t
+
+#define PRIxFAST32   "lx"
+//hexadecimal printf format for uint_fast32_t
+
+#define PRIXFAST32   "lX"
+//uppercase hexadecimal printf format for uint_fast32_t
+
+#define PRIxFAST8   "x"
+//hexadecimal printf format for uint_fast8_t
+
+#define PRIXFAST8   "X"
+//uppercase hexadecimal printf format for uint_fast8_t
+
+#define PRIxLEAST16   "x"
+//hexadecimal printf format for uint_least16_t
+
+#define PRIXLEAST16   "X"
+//uppercase hexadecimal printf format for uint_least16_t
+
+#define PRIxLEAST32   "lx"
+//hexadecimal printf format for uint_least32_t
+
+#define PRIXLEAST32   "lX"
+//uppercase hexadecimal printf format for uint_least32_t
+
+#define PRIxLEAST8   "x"
+//hexadecimal printf format for uint_least8_t
+
+#define PRIXLEAST8   "X"
+//uppercase hexadecimal printf format for uint_least8_t
+
+#define PRIxPTR   PRIx16
+//hexadecimal printf format for uintptr_t
+
+#define PRIXPTR   PRIX16
+//uppercase hexadecimal printf format for uintptr_t
+
+#define SCNd16   "d"
+//decimal scanf format for int16_t
+
+#define SCNd32   "ld"
+//decimal scanf format for int32_t
+
+#define SCNd8   "hhd"
+//decimal scanf format for int8_t
+
+#define SCNdFAST16   "d"
+//decimal scanf format for int_fast16_t
+
+#define SCNdFAST32   "ld"
+//decimal scanf format for int_fast32_t
+
+#define SCNdFAST8   "hhd"
+//decimal scanf format for int_fast8_t
+
+#define SCNdLEAST16   "d"
+//decimal scanf format for int_least16_t
+
+#define SCNdLEAST32   "ld"
+//decimal scanf format for int_least32_t
+
+#define SCNdLEAST8   "hhd"
+//decimal scanf format for int_least8_t
+
+#define SCNdPTR   SCNd16
+//decimal scanf format for intptr_t
+
+#define SCNi16   "i"
+//generic-integer scanf format for int16_t
+
+#define SCNi32   "li"
+//generic-integer scanf format for int32_t
+
+#define SCNi8   "hhi"
+//generic-integer scanf format for int8_t
+
+#define SCNiFAST16   "i"
+//generic-integer scanf format for int_fast16_t
+
+#define SCNiFAST32   "li"
+//generic-integer scanf format for int_fast32_t
+
+#define SCNiFAST8   "hhi"
+//generic-integer scanf format for int_fast8_t
+
+#define SCNiLEAST16   "i"
+//generic-integer scanf format for int_least16_t
+
+#define SCNiLEAST32   "li"
+//generic-integer scanf format for int_least32_t
+
+#define SCNiLEAST8   "hhi"
+//generic-integer scanf format for int_least8_t
+
+#define SCNiPTR   SCNi16
+//generic-integer scanf format for intptr_t
+
+#define SCNo16   "o"
+//octal scanf format for uint16_t
+
+#define SCNo32   "lo"
+//octal scanf format for uint32_t
+
+#define SCNo8   "hho"
+//octal scanf format for uint8_t
+
+#define SCNoFAST16   "o"
+//octal scanf format for uint_fast16_t
+
+#define SCNoFAST32   "lo"
+//octal scanf format for uint_fast32_t
+
+#define SCNoFAST8   "hho"
+//octal scanf format for uint_fast8_t
+
+#define SCNoLEAST16   "o"
+//octal scanf format for uint_least16_t
+
+#define SCNoLEAST32   "lo"
+//octal scanf format for uint_least32_t
+
+#define SCNoLEAST8   "hho"
+//octal scanf format for uint_least8_t
+
+#define SCNoPTR   SCNo16
+//octal scanf format for uintptr_t
+
+#define SCNu16   "u"
+//decimal scanf format for uint16_t
+
+#define SCNu32   "lu"
+//decimal scanf format for uint32_t
+
+#define SCNu8   "hhu"
+//decimal scanf format for uint8_t
+
+#define SCNuFAST16   "u"
+//decimal scanf format for uint_fast16_t
+
+#define SCNuFAST32   "lu"
+//decimal scanf format for uint_fast32_t
+
+#define SCNuFAST8   "hhu"
+//decimal scanf format for uint_fast8_t
+
+#define SCNuLEAST16   "u"
+//decimal scanf format for uint_least16_t
+
+#define SCNuLEAST32   "lu"
+//decimal scanf format for uint_least32_t
+
+#define SCNuLEAST8   "hhu"
+//decimal scanf format for uint_least8_t
+
+#define SCNuPTR   SCNu16
+//decimal scanf format for uintptr_t
+
+#define SCNx16   "x"
+//hexadecimal scanf format for uint16_t
+
+#define SCNx32   "lx"
+//hexadecimal scanf format for uint32_t
+
+#define SCNx8   "hhx"
+//hexadecimal scanf format for uint8_t
+
+#define SCNxFAST16   "x"
+//hexadecimal scanf format for uint_fast16_t
+
+#define SCNxFAST32   "lx"
+//hexadecimal scanf format for uint_fast32_t
+
+#define SCNxFAST8   "hhx"
+//hexadecimal scanf format for uint_fast8_t
+
+#define SCNxLEAST16   "x"
+//hexadecimal scanf format for uint_least16_t
+
+#define SCNxLEAST32   "lx"
+//hexadecimal scanf format for uint_least32_t
+
+#define SCNxLEAST8   "hhx"
+//hexadecimal scanf format for uint_least8_t
+
+#define SCNxPTR   SCNx16
+//hexadecimal scanf format for uintptr_t
