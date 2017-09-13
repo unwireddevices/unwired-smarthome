1f53d4e26b8524ca0983556700bf215b1ed470b9
vvzvlad <vvzvlad@gmail.com>
2017-09-13 14:10:10 +0300

    cosmetic messages

--- udrouter/router.lua
+++ udrouter/router.lua
@@ -365,7 +365,7 @@ function ipv6_adress_parse(ipv6_adress)
 	if (end_1 ~= nil) then
 		return a[1]..a[2]..a[3]..a[4]..a[5]..a[6]..a[7]..a[8]..a[9]..a[10]..a[11]..a[12]..a[13]..a[14]..a[15]..a[16]
 	else
-		print("IV6P: Adress parse error"..ipv6_adress)
+		print("IV6P: Adress parse error "..ipv6_adress)
 		return nil
 	end
 end
