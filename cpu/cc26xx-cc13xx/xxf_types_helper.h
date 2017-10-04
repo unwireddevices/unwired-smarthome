f0a85ac5981fe27a5270a1fe2b1ba962bb0fd698
vvzvlad <vvzvlad@gmail.com>
2017-10-04 19:04:41 +0300

    cosmetic

--- cpu/cc26xx-cc13xx/xxf_types_helper.h
+++ cpu/cc26xx-cc13xx/xxf_types_helper.h
@@ -1,15 +1,15 @@
+#define PRId8   "d"
+//decimal printf format for int8_t
+
 #define PRId16   "d"
 //decimal printf format for int16_t
 
 #define PRId32   "ld"
 //decimal printf format for int32_t
 
-#define PRId8   "d"
-//decimal printf format for int8_t
 
-
-#define PRIdPTR   PRId16
-//decimal printf format for intptr_t
+#define PRIi8   "i"
+//integer printf format for int8_t
 
 #define PRIi16   "i"
 //integer printf format for int16_t
@@ -17,12 +17,8 @@
 #define PRIi32   "li"
 //integer printf format for int32_t
 
-#define PRIi8   "i"
-//integer printf format for int8_t
-
-#define PRIiPTR   PRIi16
-//integer printf format for intptr_t
-
+#define PRIi64   "lli"
+//integer printf format for int32_t
 
 
 #define PRIu8   "u"
@@ -33,10 +29,6 @@
 
 #define PRIu32   "lu"
 //decimal printf format for uint32_t
-
-#define PRIuPTR   PRIu16
-//decimal printf format for uintptr_t
-
 
 
 #define PRIx8   "x"
@@ -65,147 +57,3 @@
 //uppercase hexadecimal printf format for uint32_t
 
 
-
-#define PRIxPTR   PRIx16
-//hexadecimal printf format for uintptr_t
-
-#define PRIXPTR   PRIX16
-//uppercase hexadecimal printf format for uintptr_t
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#define SCNd16   "d"
-//decimal scanf format for int16_t
-
-#define SCNd32   "ld"
-//decimal scanf format for int32_t
-
-#define SCNd8   "hhd"
-//decimal scanf format for int8_t
-
-#define SCNdFAST16   "d"
-//decimal scanf format for int_fast16_t
-
-#define SCNdFAST32   "ld"
-//decimal scanf format for int_fast32_t
-
-#define SCNdFAST8   "hhd"
-//decimal scanf format for int_fast8_t
-
-#define SCNdLEAST16   "d"
-//decimal scanf format for int_least16_t
-
-#define SCNdLEAST32   "ld"
-//decimal scanf format for int_least32_t
-
-#define SCNdLEAST8   "hhd"
-//decimal scanf format for int_least8_t
-
-#define SCNdPTR   SCNd16
-//decimal scanf format for intptr_t
-
-#define SCNi16   "i"
-//generic-integer scanf format for int16_t
-
-#define SCNi32   "li"
-//generic-integer scanf format for int32_t
-
-#define SCNi8   "hhi"
-//generic-integer scanf format for int8_t
-
-#define SCNiFAST16   "i"
-//generic-integer scanf format for int_fast16_t
-
-#define SCNiFAST32   "li"
-//generic-integer scanf format for int_fast32_t
-
-#define SCNiFAST8   "hhi"
-//generic-integer scanf format for int_fast8_t
-
-#define SCNiLEAST16   "i"
-//generic-integer scanf format for int_least16_t
-
-#define SCNiLEAST32   "li"
-//generic-integer scanf format for int_least32_t
-
-#define SCNiLEAST8   "hhi"
-//generic-integer scanf format for int_least8_t
-
-#define SCNiPTR   SCNi16
-//generic-integer scanf format for intptr_t
-
-
-#define SCNu16   "u"
-//decimal scanf format for uint16_t
-
-#define SCNu32   "lu"
-//decimal scanf format for uint32_t
-
-#define SCNu8   "hhu"
-//decimal scanf format for uint8_t
-
-#define SCNuFAST16   "u"
-//decimal scanf format for uint_fast16_t
-
-#define SCNuFAST32   "lu"
-//decimal scanf format for uint_fast32_t
-
-#define SCNuFAST8   "hhu"
-//decimal scanf format for uint_fast8_t
-
-#define SCNuLEAST16   "u"
-//decimal scanf format for uint_least16_t
-
-#define SCNuLEAST32   "lu"
-//decimal scanf format for uint_least32_t
-
-#define SCNuLEAST8   "hhu"
-//decimal scanf format for uint_least8_t
-
-#define SCNuPTR   SCNu16
-//decimal scanf format for uintptr_t
-
-#define SCNx16   "x"
-//hexadecimal scanf format for uint16_t
-
-#define SCNx32   "lx"
-//hexadecimal scanf format for uint32_t
-
-#define SCNx8   "hhx"
-//hexadecimal scanf format for uint8_t
-
-#define SCNxFAST16   "x"
-//hexadecimal scanf format for uint_fast16_t
-
-#define SCNxFAST32   "lx"
-//hexadecimal scanf format for uint_fast32_t
-
-#define SCNxFAST8   "hhx"
-//hexadecimal scanf format for uint_fast8_t
-
-#define SCNxLEAST16   "x"
-//hexadecimal scanf format for uint_least16_t
-
-#define SCNxLEAST32   "lx"
-//hexadecimal scanf format for uint_least32_t
-
-#define SCNxLEAST8   "hhx"
-//hexadecimal scanf format for uint_least8_t
-
-#define SCNxPTR   SCNx16
-//hexadecimal scanf format for uintptr_t
