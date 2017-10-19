f63a55fbdeb1e90b7872740bb4418d50e806193b
vvzvlad <vvzvlad@gmail.com>
2017-10-19 17:35:48 +0300

    new make big update

---
+++ tools/selective_clean.lua
@@ -0,0 +1,136 @@
+--[[
+/*
+ * Copyright (c) 2016, Unwired Devices LLC - http://www.unwireddevices.com/
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Unwired Devices nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */ ]]--
+
+local current_leaf_mode = arg[1]
+local current_cpu = arg[2]
+local last_leaf_mode
+local last_cpu
+local last_modes = {}
+local filename_last_modes = "/tmp/unwired_last_mode"
+
+
+function table.val_to_str ( v )
+   if "string" == type( v ) then
+      v = string.gsub( v, "\n", "\\n" )
+      if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+         return "'" .. v .. "'"
+      end
+      return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+   end
+   return "table" == type( v ) and table.tostring( v ) or tostring( v )
+end
+function table.key_to_str ( k )
+   if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+      return k
+   end
+   return "[" .. table.val_to_str( k ) .. "]"
+end
+
+function table.tostring( tbl )
+   local result, done = {}, {}
+   for k, v in ipairs( tbl ) do
+      table.insert( result, table.val_to_str( v ) )
+      done[ k ] = true
+   end
+   for k, v in pairs( tbl ) do
+      if not done[ k ] then
+         table.insert( result, table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+      end
+   end
+   return "{" .. table.concat( result, "," ) .. "}"
+end
+
+function table.read(filename)
+   local f, err = io.open(filename,"r")
+   if not f then
+   	  local nil_table = {}
+      return nil_table, err
+   end
+   local tbl = assert(loadstring("return " .. f:read("*a")))
+   f:close()
+   return tbl()
+end
+
+function table.save(tbl, filename)
+   local f,err = io.open(filename,"w")
+   local nil_table = {}
+   if not f then
+      return nil,err
+   end
+   f:write(table.tostring(tbl))
+   f:close()
+   return true
+end
+
+
+
+
+
+local os_exit_code = 1
+if (current_leaf_mode == nil or current_cpu == nil) then
+	print("Arg error")
+	os.exit(os_exit_code)
+end
+
+last_modes, err = table.read(filename_last_modes)
+
+if (last_modes ~= nil) then
+	if (last_modes.leaf_mode ~= nil and last_modes.cpu ~= nil) then
+		last_leaf_mode = last_modes.leaf_mode
+		last_cpu = last_modes.cpu
+	end
+end
+
+if (err == nil) then
+	if (last_leaf_mode == current_leaf_mode) and (last_cpu == current_cpu) then
+		os_exit_code = 0
+	end
+end
+
+local current_modes = {}
+current_modes.leaf_mode = current_leaf_mode
+current_modes.cpu = current_cpu
+table.save(current_modes, filename_last_modes)
+os.exit(os_exit_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
